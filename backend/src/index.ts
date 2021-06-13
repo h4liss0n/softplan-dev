1d335013586ce401cbceaa6805d0c96ac10b1a36
Halisson Skalee <47496754+h4liss0n@users.noreply.github.com>
2021-06-13 14:35:39 -0300

    swagger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,22 +5,45 @@ import * as morgan from 'morgan';
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import routes from "./router";
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./public/swagger.json');
 
 
+var options = {
+    explorer: true
+};
 
 
 
 createConnection().then(async connection => {
     const app = express();
-    const router = express.Router();    
+    const router = express.Router();
     app.use(morgan('tiny'))
     app.use(bodyParser.json());
     
+    app.use('/public', express.static(__dirname + '/public'));
 
-    
+
+
+
+    app.use(
+        "/docs",
+        swaggerUi.serve,
+        swaggerUi.setup(undefined, {
+            swaggerOptions: {
+                url: "/public/swagger.json",
+            },
+        })
+    );
+
+
     app.use(cors())
-    app.use(routes);    
+    app.use(routes);
     app.listen('8080');
+
+
+
+
 
     console.log(`localhost:8080`);
 
